26fb8536c62f95bb06eb4ee550ade96546fd1a73
ErickESC <eekoston@gmail.com>
2020-07-08 01:39:14 -0500

    AlumnoControllerTest realiza prueba exitosa y no exitosa de create

--- src/test/java/mx/uam/tsis/ejemplobackend/servicios/AlumnoControllerIntegrationTest.java
+++ src/test/java/mx/uam/tsis/ejemplobackend/servicios/AlumnoControllerIntegrationTest.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import mx.uam.tsis.ejemplobackend.datos.AlumnoRepository;
 import mx.uam.tsis.ejemplobackend.negocio.modelo.Alumno;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -43,7 +44,35 @@ public class AlumnoControllerIntegrationTest {
 		
 		// Aqui se puede hacer cosas para preparar sus casos de prueba, incluyendo
 		// agregar datos a la BD
+		Alumno alumno = new Alumno();
+		alumno.setCarrera("Computación");
+		alumno.setMatricula(5678);
+		alumno.setNombre("PerryElOrnitorringo");
 		
+		// Creo el encabezado
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("content-type",MediaType.APPLICATION_JSON.toString());
+		
+		// Creo la petición con el alumno como body y el encabezado
+		HttpEntity <Alumno> request = new HttpEntity <> (alumno, headers);
+		
+		restTemplate.exchange("/alumnos", HttpMethod.POST, request, Alumno.class);
+	}
+	
+	@AfterEach
+	public void destroy() {
+		// Aqui se puede hacer cosas para deshacer lo que se realizo antes de los casos de prueba
+		// Elimino al al alumno
+		int matricula = 5678;
+		
+		// Creo el encabezado
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("content-type",MediaType.APPLICATION_JSON.toString());
+		
+		// Creo la petición con el alumno como body y el encabezado
+		HttpEntity <Integer> request = new HttpEntity <> (matricula, headers);
+		
+		restTemplate.exchange("/alumnos/"+matricula, HttpMethod.DELETE, request, Integer.class);
 	}
 	
 	/*
@@ -53,7 +82,7 @@ public class AlumnoControllerIntegrationTest {
 	@Test
 	public void testCreate201() {
 		
-		// Creo el alumno que voy a enviar
+		// Creo el alumno que voy a enviar con datos distintos al creado en prepare
 		Alumno alumno = new Alumno();
 		alumno.setCarrera("Computación");
 		alumno.setMatricula(12345678);
@@ -76,11 +105,26 @@ public class AlumnoControllerIntegrationTest {
 		// Corroboro que en la base de datos se guardó el alumno
 		Optional <Alumno> optAlumno = alumnoRepository.findById(12345678);
 		assertEquals(alumno,optAlumno.get());
-		
 	}
 
 	@Test
 	public void testCreate400() {
+		// Creo el alumno que voy a enviar pero sin matricula
+		Alumno alumno = new Alumno();
+		alumno.setCarrera("Computacion");
 		
+		alumno.setNombre("Chuchin");
+
+		// Creo el encabezado
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("content-type",MediaType.APPLICATION_JSON.toString());
+		
+		// Creo la petición con el alumno como body y el encabezado
+		HttpEntity <Alumno> request = new HttpEntity <> (alumno, headers);
+		
+		ResponseEntity <Alumno> responseEntity = restTemplate.exchange("/alumnos", HttpMethod.POST, request, Alumno.class);
+		
+		// Corroboro que el endpoint me regresa el estatus esperado
+		assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
 	}
 }
